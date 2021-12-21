d85f2d5e7677e27cb726b2961b05510ddf57131f
nikhiltavar <nikhiltavarcr7@gmail.com>
2021-12-22 01:57:55 +0530

    4th

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('news/', views.news),
-    
+    path('news/', views.news, name = 'news'),
+    path('apinews/', views.apinews, name = 'apinews')
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import News, Tags
+import requests
 
 def news(request):
     news = News.objects.first()
@@ -7,5 +8,23 @@ def news(request):
         'news':news
     }
     return render(request, 'news/news.html', data)
+
+def apinews(request):
+    url = "https://covid-19-news.p.rapidapi.com/v1/covid"
+    querystring = {"q":"covid","lang":"en","sort_by":"relevancy","country":"in","media":"True"}
+    headers = {
+    'x-rapidapi-host': "covid-19-news.p.rapidapi.com",
+    'x-rapidapi-key': "8200d19c55msh37593ee279e19c7p122d33jsna7c4444144f1"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()  
+    articles = data['articles']
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'news/apinews.html', context)
+
+
 
 
