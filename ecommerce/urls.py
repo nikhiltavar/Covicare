e6fae896bf86d63ca8780fad7d3dcee41b51d0a3
nikhiltavar <nikhiltavarcr7@gmail.com>
2022-04-09 21:46:10 +0530

    Ecom Base Template Created

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,14 +2,9 @@ from django.urls import path , include
 from . import views
 
 
-
-
-
-
-
-
-
 urlpatterns = [
     path('shop/', views.shopHome, name = 'shop'),
+    path('shop/checkout/', views.checkout, name = 'checkout'),
+    path('shop/cart/', views.cart, name = 'cart'),
 ]
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def shopHome(request):
     return render (request, 'ecommerce/shophome.html')
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'ecommerce/checkout.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'ecommerce/cart.html', context)
+
+
