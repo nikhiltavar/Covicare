cf7727c09886e09c8b21cc3358b9595f8a8c9b11
nikhiltavar <nikhiltavarcr7@gmail.com>
2022-04-10 15:25:26 +0530

    Dynamically added Data In shop, cart and checkout

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -15,11 +15,20 @@ class Customer (models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
+    image = models.ImageField(null=True, blank=True)
 
 
     def __str__(self):
         return self.name
 
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -30,6 +39,17 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey (Product, on_delete=models.SET_NULL, null=True)
@@ -37,6 +57,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity 
+        return total
 
 
 class ShippingAddress(models.Model):
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,19 +1,49 @@
 from django.shortcuts import render
+from .models import *
 
 def shopHome(request):
+    products = Product.objects.all()
 
-    
-    context = {}
+    context = { 
+        'products': products,
+    }
     return render (request, 'ecommerce/shophome.html', context)
 
 
+
+    
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+                        
+    else:
+        #Create Empty cart for now for none-logged in users
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+             
+    context = {
+        'items':items, 
+        'order':order,
+    }
     return render(request, 'ecommerce/checkout.html', context)
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'ecommerce/cart.html', context)
 
 
